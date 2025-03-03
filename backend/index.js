e6e3f59ae34f582c18c8685805718ca7b08b540e
Devenvaruv <devenvaruv@gmail.com>
2025-03-02 19:42:29 -0800

    updated mysql database

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,11 @@ import cors from "cors"
 const app = express();
 
 const db = mysql.createConnection({
-    host: "db",
-    user: "user",
-    password: "pass",
-    database: "test"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 })
 
 app.use(express.json())//return json data using the api server postman
